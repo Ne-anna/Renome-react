6a85d5c8cd0f237195b253dcca59c12f6efd3bfe
Ne-anna <annabusele@inbox.lv>
2022-05-10 16:09:05 +0300

    Change about.css/index.css,improve About.js/App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@
 import About from './Components/Organisms/About/About';
 
 function App() {
+  const articles = { title: 'about', subtitle: 'We create delicious memories', body: "enome it is renowned restaurant delights the senses with elegant ambiance, gracious service and delectable menus in the heart of Manhattan’s. The distinctive setting boasts contemporary furnishings and finishes atop the restaurant’s grand modern architecture. Renome offers the room for private dining where guests enjoy creative foods and sumptuous late night desserts. Is one of the few Manhattan restaurants to have a coveted 3-star Michelin rating.", id: 1 }
+
   return (
     <div className="App">
-      <About/>
+      <About {...articles}/>
     </div>
   );
 }
