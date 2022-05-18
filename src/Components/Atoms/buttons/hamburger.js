6fbd826509f6292a25ea5e1c360ffeb3d14e4951
Ne-anna <annabusele@inbox.lv>
2022-05-18 12:28:41 +0300

    Add menu opening

--- src/Components/Atoms/buttons/hamburger.js
+++ src/Components/Atoms/buttons/hamburger.js
@@ -1,18 +1,22 @@
 import './hamburger.scss';
 import React, { useState } from 'react';
-// import Menu from 'src/Components/Molecules/menu/menu';
+import Menu from 'src/Components/Molecules/menu/menu';
 
 const Hamburger = () => {
-    const [isActive, setActive] = useState(false);
 
+    const [isActive, setActive] = useState(false);
     const toggleClass = () => {
-        setActive(!isActive);
+        (setActive(!isActive))
     };
+
     return (
-        <div className={`navigation__hamburger${isActive ? " active" : ""}`} onClick={toggleClass}>
+        <div className={`navigation__hamburger${isActive ? " active" : ""}`} onClick={toggleClass} >
             <span className="navigation__hamburger-bar"></span>
             <span className="navigation__hamburger-bar"></span>
             <span className="navigation__hamburger-bar"></span>
+            {isActive && (
+                <Menu />
+            )}
         </div>
     );
 }
