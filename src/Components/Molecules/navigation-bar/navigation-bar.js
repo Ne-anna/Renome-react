1266234ffa0904ffe66856fc3608310e21691444
Ne-anna <annabusele@inbox.lv>
2022-05-18 10:02:41 +0300

    Create navigation and pass data from json file

---
+++ src/Components/Molecules/navigation-bar/navigation-bar.js
@@ -0,0 +1,21 @@
+import './navigation-bar.scss';
+import jsonFile from 'src/db.json';
+import Hamburger from 'src/Components/Atoms/buttons/hamburger';
+
+const NavigationBar = () => {
+    const navigation = jsonFile.navigation;
+
+    return (
+        <nav>
+            <h2 className="navigation--left">{navigation.logo}</h2>
+            <div className="navigation--right">
+                <img className="navigation__cart" src={navigation.cart} alt={navigation.altTagCart} />
+                <p className="navigation__count">{navigation.count}</p>
+                <img className="navigation__divide" src={navigation.divider} alt={navigation.altTagDivider} />
+                <Hamburger />
+            </div>
+        </nav>
+    );
+}
+
+export default NavigationBar;
