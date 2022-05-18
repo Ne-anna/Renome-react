9ffc97787d60b3eb78b2e94f8936c74a2ca1f698
Ne-anna <annabusele@inbox.lv>
2022-05-18 10:01:53 +0300

    Create navigation.js and navigation.scss

---
+++ src/Components/Organisms/Navigation/navigation.js
@@ -0,0 +1,11 @@
+import './navigation.scss';
+import NavigationBar from 'src/Components/Molecules/navigation-bar/navigation-bar';
+
+const Navigation = () => {
+
+    return (
+        <NavigationBar/>
+    );
+}
+
+export default Navigation;
