0e1aa9c23028bfc52c5ac47ef845eda6e8965774
Ne-anna <annabusele@inbox.lv>
2022-05-19 10:31:59 +0300

    Fix menu.js

--- src/Components/Molecules/menu/menu.js
+++ src/Components/Molecules/menu/menu.js
@@ -2,23 +2,18 @@ import './menu.scss';
 import jsonFile from 'src/db.json';
 import FeaturesNext from 'src/Components/Atoms/buttons/features-next';
 // import SubMenu from '../sub-menu.js/sub-menu';
-// import { useState } from 'react';
 // import Search from 'src/Components/Atoms/buttons/search-button';
 
 const Menu = () => {
     const menu = jsonFile.menu;
-    // const [isActive,setIsActive] = useState(false);
-    // const handleToggle =() => {
-    //     setIsActive(!isActive);
-    // }
-   
+
     return (
         <div className="navigation__menu">
             {
                 menu.map((post, index) => {
                     return (
-                        <li className={`navigation__item ${post.isSubMenu ? 'navigation__item--features' : 'navigation__item__submenu'}`} key={index}>
-                            <a className="navigation__link" href={post.url}>{post.title}</a>
+                        <li className={`navigation__item ${post.isSubMenu ? 'navigation__item--features' : post.title === "Search" ? 'navigation__item--search' : ''}`} key={index}>
+                            {post.title === "Search" ? <input className='navigation__input--search' placeholder={post.title} /> : <a className='navigation__link' href={post.url}>{post.title}</a>}
                             {post.isSubMenu &&
                                 <div>
                                     <FeaturesNext />
