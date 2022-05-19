4c2281729194366c370cc0cb22edb27a3c1aaed4
Ne-anna <annabusele@inbox.lv>
2022-05-19 15:46:06 +0300

    Fix outside click functionality.

--- src/Components/Atoms/buttons/hamburger.js
+++ src/Components/Atoms/buttons/hamburger.js
@@ -1,13 +1,24 @@
 import './hamburger.scss';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Menu from 'src/Components/Molecules/menu/menu';
 
 const Hamburger = () => {
-
     const [isActive, setActive] = useState(false);
+
     const toggleClass = () => {
-        (setActive(!isActive))
+        setActive(!isActive)
     };
+
+    const wrapperRef = useRef();
+    useEffect(() => {
+        const closeMenu = e => {
+            if (!wrapperRef.current?.contains(e.target)) {
+                setActive(false)
+            }
+        }
+        document.body.addEventListener("mousedown", closeMenu);
+
+    }, [wrapperRef]);
 
     if (isActive) {
         document.body.style.overflow = "hidden";
@@ -16,13 +27,16 @@ const Hamburger = () => {
     }
 
     return (
-        <div className={`navigation__hamburger${isActive ? " active" : ""}`} onClick={toggleClass} >
-            <span className="navigation__hamburger-bar"></span>
-            <span className="navigation__hamburger-bar"></span>
-            <span className="navigation__hamburger-bar"></span>
+        <div ref={wrapperRef} className={`navigation__hamburger${isActive ? " active" : ""}`}  >
+            <div onClick={toggleClass}>
+                <span className="navigation__hamburger-bar"></span>
+                <span className="navigation__hamburger-bar"></span>
+                <span className="navigation__hamburger-bar"></span>
+            </div>
             {isActive && (
                 <Menu />
             )}
+
         </div>
     );
 }
