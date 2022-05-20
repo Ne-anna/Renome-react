1aa367ee11fc362c8185bbda2272caaddd3ceee0
Ne-anna <annabusele@inbox.lv>
2022-05-20 14:10:53 +0300

    With click open sub-menu

--- src/Components/Molecules/menu/menu.js
+++ src/Components/Molecules/menu/menu.js
@@ -2,8 +2,12 @@ import './menu.scss';
 import jsonFile from 'src/db.json';
 import FeaturesNext from 'src/Components/Atoms/buttons/features-next-button/features-next';
 import Search from 'src/Components/Atoms/buttons/search-button/search-button';
+import React, { useState } from 'react';
+import SubMenu from '../sub-menu.js/sub-menu';
 
 const Menu = () => {
+    const [isClicked, setIsClicked] = useState(false);
+
     const menu = jsonFile.menu;
 
     return (
@@ -12,17 +16,19 @@ const Menu = () => {
                 menu.map((post, index) => {
                     return (
                         <li className={`navigation__item ${post.isSubMenu ? 'navigation__item--features' : post.title === "Search" ? 'navigation__item--search' : ''}`} key={index}>
-                            {post.title === "Search" ? <><input className='navigation__input--search' placeholder={post.title} /> <Search/> </> : <a className='navigation__link' href={post.url}>{post.title}</a>}
+                            {post.title === "Search" ? <><input className='navigation__input--search' placeholder={post.title} /> <Search /> </> : <a className='navigation__link' href={post.url}>{post.title}</a>}
                             {post.isSubMenu &&
-                                <div>
+                                <div onClick={() => setIsClicked(!isClicked)} >
                                     <FeaturesNext />
                                 </div>
                             }
                         </li>
-
                     )
                 })
             }
+            {isClicked && (
+                <SubMenu />
+            )}
         </div>
     );
 }
