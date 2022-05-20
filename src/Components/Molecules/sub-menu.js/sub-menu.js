5b2c2b5ec122e44ac551a2840258a3dfb4c86858
Ne-anna <annabusele@inbox.lv>
2022-05-20 14:10:14 +0300

    Start creating sub-menu

--- src/Components/Molecules/sub-menu.js/sub-menu.js
+++ src/Components/Molecules/sub-menu.js/sub-menu.js
@@ -1,11 +1,28 @@
-const SubMenu = () => {
-    return (
-        <div>
-            <p className="">cat</p>
-            <p className="">dog</p>
-            <p className="">duck</p>
-        </div>
+import jsonFile from 'src/db.json';
+import './sub-menu.scss';
 
+const SubMenu = () => {
+    const menu = jsonFile.menu;
+
+    return (
+        <div className="sub-navigation">
+            {
+                menu.map((post => {
+                    return (
+                        (typeof (post.subMenu) == 'object') ?
+                            <>
+                                {
+                                    post.subMenu.map((post) =>
+                                    <div className='sub-navigation__item'>
+                                        <p className="sub-navigation__text"> {post.title} </p>
+                                        </div>)
+                                }
+                            </>
+                            : null
+                    )
+                }))
+            }
+        </div>
     );
 }
 
