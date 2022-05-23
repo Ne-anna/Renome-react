a18874b38a4743a75ab3d0ee0e9fcb7b96d8c312
Ne-anna <annabusele@inbox.lv>
2022-05-23 12:17:02 +0300

    Create sub-menu button back

---
+++ src/Components/Atoms/buttons/submenu-back-button.js/submenu-back.js
@@ -0,0 +1,11 @@
+import './submenu-back.scss'
+
+const SubMenuBack = () => {
+    return (
+        <button className="sub-navigation__button--back">
+            <img src={process.env.PUBLIC_URL + '/assets/icons/icon-back-submenu.svg'} alt="back icon" />
+        </button>
+    );
+}
+ 
+export default SubMenuBack;
