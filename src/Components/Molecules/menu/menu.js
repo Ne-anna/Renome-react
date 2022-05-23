f3907b1334c04500a0f1344304dc8cf7c7175ff6
Ne-anna <annabusele@inbox.lv>
2022-05-23 12:21:10 +0300

    Create open/close sub-menu,outside click

--- src/Components/Molecules/menu/menu.js
+++ src/Components/Molecules/menu/menu.js
@@ -2,27 +2,19 @@ import './menu.scss';
 import jsonFile from 'src/db.json';
 import FeaturesNext from 'src/Components/Atoms/buttons/features-next-button/features-next';
 import Search from 'src/Components/Atoms/buttons/search-button/search-button';
-import React, { useState } from 'react';
-import SubMenu from '../sub-menu.js/sub-menu';
 
-const Menu = () => {
-    const [isClicked, setIsClicked] = useState(false);
-
-    const openSubMenu = () => {
-        setIsClicked(!isClicked)
-    }
-
+const Menu = (props) => {
     const menu = jsonFile.menu;
 
     return (
-        <div className="navigation__menu">
+        <div className={`navigation__menu`}>
             {
                 menu.map((post, index) => {
                     return (
                         <li className={`navigation__item ${post.isSubMenu ? 'navigation__item--features' : post.title === "Search" ? 'navigation__item--search' : ''}`} key={index}>
                             {post.title === "Search" ? <><input className='navigation__input--search' placeholder={post.title} /> <Search /> </> : <a className='navigation__link' href={post.url}>{post.title}</a>}
                             {post.isSubMenu &&
-                                <div onClick={openSubMenu} >
+                                <div onClick={props.subMenuOpen} >
                                     <FeaturesNext />
                                 </div>
                             }
@@ -30,9 +22,7 @@ const Menu = () => {
                     )
                 })
             }
-            {isClicked && (
-                <SubMenu />
-            )}
+        
         </div>
     );
 }
