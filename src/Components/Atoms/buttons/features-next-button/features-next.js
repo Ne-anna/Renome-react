5ca8341886fdd193a22c19b4d2cb57f2f496a2f3
Ne-anna <annabusele@inbox.lv>
2022-05-23 13:45:13 +0300

    Change scss

--- src/Components/Atoms/buttons/features-next-button/features-next.js
+++ src/Components/Atoms/buttons/features-next-button/features-next.js
@@ -3,7 +3,7 @@ import './features-next.scss';
 const FeaturesNext = () => {
     return (
         <button className="navigation__button--features">
-            <img className="features__img" src={process.env.PUBLIC_URL + '/assets/icons/icon-next-features.svg'} alt="next icon" />
+            <img src={process.env.PUBLIC_URL + '/assets/icons/icon-next-features.svg'} alt="next icon" />
         </button>
     );
 }
--- src/Components/Atoms/buttons/search-button/search-button.js
+++ src/Components/Atoms/buttons/search-button/search-button.js
@@ -3,7 +3,7 @@ import './search-button.scss';
 const Search = () => {
     return (
         <button className="navigation__button--search">
-            <img className="navigation__img" src={process.env.PUBLIC_URL + '/assets/icons/icon-search.svg'} alt="search icon" />
+            <img src={process.env.PUBLIC_URL + '/assets/icons/icon-search.svg'} alt="search icon" />
         </button>
     );
 }
