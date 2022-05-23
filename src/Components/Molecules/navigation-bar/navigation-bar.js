a1c7b1af439d144ed42ad0c569a3b5c741960842
Ne-anna <annabusele@inbox.lv>
2022-05-23 17:22:56 +0300

    Add Cart dropdown functionality.

--- src/Components/Molecules/navigation-bar/navigation-bar.js
+++ src/Components/Molecules/navigation-bar/navigation-bar.js
@@ -1,18 +1,41 @@
 import './navigation-bar.scss';
 import jsonFile from 'src/db.json';
+import React, { useState, useRef, useEffect } from 'react';
+import CartDropDown from '../Cart-drop-down/cart-drop-down';
 import Hamburger from 'src/Components/Atoms/buttons/hamburger-button/hamburger';
 
 const NavigationBar = () => {
     const navigation = jsonFile.navigation;
 
+    const [isopenCart, setOpenCart] = useState(false);
+
+    const openCart = () => {
+        setOpenCart(!isopenCart)
+    }
+
+    const cartRef = useRef();
+    useEffect(() => {
+        const closeMenu = e => {
+            if (!cartRef.current?.contains(e.target)) {
+                setOpenCart(false)
+            }
+        }
+        document.body.addEventListener("mousedown", closeMenu);
+
+    }, [cartRef]);
+
     return (
         <nav>
             <h2 className="navigation--left">{navigation.logo}</h2>
-            <div className="navigation--right">
-                <img className="navigation__cart" src={navigation.cart} alt={navigation.altTagCart} />
+            <div  className="navigation--right">
+                <img ref={cartRef} className="navigation__cart" src={navigation.cart} alt={navigation.altTagCart} onClick={openCart} />
                 <p className="navigation__count">{navigation.count}</p>
                 <img className="navigation__divide" src={navigation.divider} alt={navigation.altTagDivider} />
                 <Hamburger />
+                {isopenCart && (
+                    <CartDropDown />
+                )
+                }
             </div>
         </nav>
     );
