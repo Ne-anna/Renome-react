5e21fab3c08550dadfb26e00061b3b61ee1a50fe
Ne-anna <annabusele@inbox.lv>
2022-05-23 12:18:42 +0300

    Implement in sub-menu button back and 'back' word.

--- src/Components/Molecules/sub-menu.js/sub-menu.js
+++ src/Components/Molecules/sub-menu.js/sub-menu.js
@@ -1,11 +1,17 @@
 import jsonFile from 'src/db.json';
+import SubMenuBack from 'src/Components/Atoms/buttons/submenu-back-button.js/submenu-back';
 import './sub-menu.scss';
 
-const SubMenu = () => {
+const SubMenu = (props) => {
     const menu = jsonFile.menu;
 
     return (
         <div className="sub-navigation">
+            <div className="sub-navigation__item--back">
+                <div onClick={props.subMenuClose}> <SubMenuBack /></div>
+
+                <p className="sub-navigation__text--back" onClick={props.subMenuClose}>Back</p>
+            </div>
             {
                 menu.map((post => {
                     return (
@@ -13,8 +19,8 @@ const SubMenu = () => {
                             <>
                                 {
                                     post.subMenu.map((post) =>
-                                    <div className='sub-navigation__item'>
-                                        <p className="sub-navigation__text"> {post.title} </p>
+                                        <div className='sub-navigation__item'>
+                                            <p className="sub-navigation__text"> {post.title} </p>
                                         </div>)
                                 }
                             </>
