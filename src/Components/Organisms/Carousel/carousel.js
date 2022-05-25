aeae46dabc13a8a67a64b4c61b6700710518504d
Ne-anna <annabusele@inbox.lv>
2022-05-25 13:43:52 +0300

    Change naming, add text

--- src/Components/Organisms/Carousel/carousel.js
+++ src/Components/Organisms/Carousel/carousel.js
@@ -1,21 +1,18 @@
-import CarouselImages from "src/Components/Molecules/carousel-images/carousel-images";
+import CarouselComponent from "src/Components/Molecules/carousel-content/carousel-content";
 import CarouselNextButton from "src/Components/Atoms/buttons/carousel-next-button/carousel-next-button";
 import CarouselPreviousButton from "src/Components/Atoms/buttons/carousel-previous-button/carousel-previous-button";
-import './carousel.scss';
+import "./carousel.scss";
 
 const Carousel = () => {
   return (
     <div className="carousel">
-      <div className="carousel__item">
-        <CarouselImages />
-      </div>
+      <CarouselComponent />
       <div className="carousel__navigation">
         <CarouselPreviousButton />
         <CarouselNextButton />
       </div>
     </div>
-
   );
-}
+};
 
 export default Carousel;
