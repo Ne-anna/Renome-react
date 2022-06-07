6cb401b29cc1b474052125da9d353efad8de41f0
Ne-anna <annabusele@inbox.lv>
2022-06-07 16:55:19 +0300

    Spacing

--- src/Components/Molecules/carousel-content/carousel-content.js
+++ src/Components/Molecules/carousel-content/carousel-content.js
@@ -1,28 +1,30 @@
 import "./carousel-content.scss";
 
-const CarouselComponent = ({ carousel, slideIndex, slideDirection, prevSlide, blockAnimation }) => {
-
+const CarouselComponent = ({
+  carousel,
+  slideIndex,
+  slideDirection,
+  prevSlide,
+  blockAnimation,
+}) => {
   return (
     <div className={"carousel__container"}>
       {carousel.map((post, index) => {
         return (
-
-          <div className={`carousel__item ${index < 1 && blockAnimation
-            ? "carousel__animation--blocked"
-            : slideIndex === index + 1
-              ? "active-" + slideDirection
-              : prevSlide === index + 1
-                ? "carousel__item--" + slideDirection
-                : "carousel__animation--active"
-            } `}
-            key={index}>
-            <div className="carousel__text" >
-              <h2 className="carousel__title">
-                {post.title}
-              </h2>
-              <h5 className="carousel__subtitle">
-                {post.subTitle}
-              </h5>
+          <div
+            className={`carousel__item ${index < 1 && blockAnimation
+                ? "carousel__animation--blocked"
+                : slideIndex === index + 1
+                  ? "active-" + slideDirection
+                  : prevSlide === index + 1
+                    ? "carousel__item--" + slideDirection
+                    : "carousel__animation--active"
+              } `}
+            key={index}
+          >
+            <div className="carousel__text">
+              <h2 className="carousel__title">{post.title}</h2>
+              <h5 className="carousel__subtitle">{post.subTitle}</h5>
             </div>
             <div className="carousel__image">
               <img src={post.path} alt={post.altTag} />
@@ -30,7 +32,7 @@ const CarouselComponent = ({ carousel, slideIndex, slideDirection, prevSlide, bl
           </div>
         );
       })}
-    </div >
+    </div>
   );
 };
 
