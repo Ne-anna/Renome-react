c8b109677281edc5aa431cfa0a01d034a718ae99
Ne-anna <annabusele@inbox.lv>
2022-06-07 16:46:16 +0300

    Remove from file

--- src/Components/Atoms/buttons/hamburger-button/hamburger.js
+++ src/Components/Atoms/buttons/hamburger-button/hamburger.js
@@ -1,40 +1,12 @@
 import "./hamburger.scss";
-import Menu from "src/Components/Molecules/menu/menu";
-import SubMenu from "src/Components/Molecules/sub-menu.js/sub-menu";
 
-const Hamburger = ({
-    toggleClass,
-    closeSubMenu,
-    openSubMenu,
-    isClicked,
-    isActive,
-    wrapperRef
-}) => {
+const Hamburger = ({ toggleClass }) => {
 
     return (
-        <div
-            ref={wrapperRef}
-            className={`navigation__hamburger${isActive
-                ? " active"
-                : isClicked
-                    ? " active" : ""
-                }`}
-        >
-            <div onClick={toggleClass}>
-                <span className="navigation__hamburger-bar"></span>
-                <span className="navigation__hamburger-bar"></span>
-                <span className="navigation__hamburger-bar"></span>
-            </div>
-            {isActive &&
-                <Menu
-                    subMenuOpen={openSubMenu}
-                />
-            }
-            {isClicked &&
-                <SubMenu
-                    subMenuClose={closeSubMenu}
-                />
-            }
+        <div onClick={toggleClass}>
+            <span className="navigation__hamburger-bar"></span>
+            <span className="navigation__hamburger-bar"></span>
+            <span className="navigation__hamburger-bar"></span>
         </div>
     );
 };
