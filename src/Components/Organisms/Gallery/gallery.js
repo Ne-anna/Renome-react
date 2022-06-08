43045b5aa7257d909a776b725e0eff43f4ece08a
Ne-anna <annabusele@inbox.lv>
2022-06-08 12:25:22 +0300

    Spacing in Gallery.js

--- src/Components/Organisms/Gallery/gallery.js
+++ src/Components/Organisms/Gallery/gallery.js
@@ -5,8 +5,12 @@ import GalleryImages from 'src/Components/Molecules/gallery-images/gallery-image
 const Gallery = (props) => {
     return (
         <div className="gallery">
-            <h1 className="gallery__title">{props.gallery.title}</h1>
-            <h5 className="gallery__subtitle">{props.gallery.subTitle}</h5>
+            <h1 className="gallery__title">
+                {props.gallery.title}
+            </h1>
+            <h5 className="gallery__subtitle">
+                {props.gallery.subTitle}
+            </h5>
             <GalleryImages />
         </div>
     );
