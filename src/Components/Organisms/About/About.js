c1a33fa4c05f458f03d4e91b2091e5b4e6281e72
Ne-anna <annabusele@inbox.lv>
2022-06-08 12:27:02 +0300

    Spacing in about.js

--- src/Components/Organisms/About/About.js
+++ src/Components/Organisms/About/About.js
@@ -5,13 +5,20 @@ import jsonFile from 'src/db.json';
 
 const About = (props) => {
     const images = jsonFile.about.images;
+
     return (
         <div className="about-container">
             <Grid images={images} />
             <div className="about">
-                <h1 className="about__title">{props.articles.title}</h1>
-                <h5 className="about__subtitle">{props.articles.subTitle}</h5>
-                <p className="about__paragraph">{props.articles.text}</p>
+                <h1 className="about__title">
+                    {props.articles.title}
+                </h1>
+                <h5 className="about__subtitle">
+                    {props.articles.subTitle}
+                </h5>
+                <p className="about__paragraph">
+                    {props.articles.text}
+                </p>
             </div>
         </div>
     );
