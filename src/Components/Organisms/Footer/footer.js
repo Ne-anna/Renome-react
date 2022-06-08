8d74ac9f5683008f776cd8d350d24ef77c7f5b05
Ne-anna <annabusele@inbox.lv>
2022-06-08 12:24:38 +0300

    Spacing

--- src/Components/Organisms/Footer/footer.js
+++ src/Components/Organisms/Footer/footer.js
@@ -5,10 +5,16 @@ import React from 'react';
 const Footer = (props) => {
     return (
         <div className="footer">
-            <h5 className="footer__title">{props.footer.title}</h5>
+            <h5 className="footer__title">
+                {props.footer.title}
+            </h5>
             <FooterIcons />
-            <h2 className="footer__logo">{props.footer.logo}</h2>
-            <h5 className="footer__copyright">{props.footer.copyright}</h5>
+            <h2 className="footer__logo">
+                {props.footer.logo}
+            </h2>
+            <h5 className="footer__copyright">
+                {props.footer.copyright}
+            </h5>
         </div>
 
     );
