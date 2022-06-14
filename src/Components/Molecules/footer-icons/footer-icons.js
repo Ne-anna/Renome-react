e020683aeb52a84b1ba160d1b6bac8a1dfbe1cd0
Ne-anna <annabusele@inbox.lv>
2022-06-14 15:56:01 +0300

    Test fetch code

--- src/Components/Molecules/footer-icons/footer-icons.js
+++ src/Components/Molecules/footer-icons/footer-icons.js
@@ -1,8 +1,35 @@
-import React from "react";
+
 import jsonFile from "src/db.json";
 import "../footer-icons/footer-icons.scss";
+import React, { useEffect } from "react";
+
 const FooterIcons = () => {
     const footerIcons = jsonFile.footer.footerIcons;
+
+    const getData = () => {
+        fetch('https://github.com/Ne-anna/Renome-react/blob/main/src/db.json'
+            , {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                },
+                mode: 'no-cors'
+            }
+        )
+            .then(response => {
+                // console.log(response)
+                response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error(error)
+            });
+    }
+    useEffect(() => {
+        getData()
+    })
 
     return (
         <div className="footer__social-media">
