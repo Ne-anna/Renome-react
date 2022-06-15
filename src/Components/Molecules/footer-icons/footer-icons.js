4242f8652abd50d7079812d7ff028d4d39d95b59
Ne-anna <annabusele@inbox.lv>
2022-06-15 10:12:20 +0300

    Other test fetch code

--- src/Components/Molecules/footer-icons/footer-icons.js
+++ src/Components/Molecules/footer-icons/footer-icons.js
@@ -6,30 +6,44 @@ import React, { useEffect } from "react";
 const FooterIcons = () => {
     const footerIcons = jsonFile.footer.footerIcons;
 
-    const getData = () => {
-        fetch('https://github.com/Ne-anna/Renome-react/blob/main/src/db.json'
-            , {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',
-                },
-                mode: 'no-cors'
-            }
-        )
-            .then(response => {
-                // console.log(response)
-                response.json();
-            })
-            .then(data => {
-                console.log(data);
-            })
-            .catch((error) => {
-                console.error(error)
+    // const getIcon = () => {
+    //     fetch('https://github.com/Ne-anna/Renome-react/blob/main/src/db.json'
+    //         , {
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //                 'Access-Control-Allow-Origin': '*',
+    //             },
+    //             mode: 'no-cors'
+    //         }
+    //     )
+    //         .then(response => {
+    //             // console.log(response)
+    //             response.json();
+    //         })
+    //         .then(data => {
+    //             console.log(data);
+    //         })
+    //         .catch((error) => {
+    //             console.error(error)
+    //         });
+    // }
+    // useEffect(() => {
+    //     getIcon()
+    // })
+
+    const getIcon = async () => {
+        const requestURL = "../../../db.json";
+        const request = new Request(requestURL);
+
+        await fetch(request)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
             });
     }
     useEffect(() => {
-        getData()
-    })
+        getIcon()
+    }, [])
 
     return (
         <div className="footer__social-media">
