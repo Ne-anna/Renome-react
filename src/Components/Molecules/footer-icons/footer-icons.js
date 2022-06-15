821fc8c2bf2371756652ea315311e6a516d9cb63
Ne-anna <annabusele@inbox.lv>
2022-06-15 15:05:47 +0300

    Remove code

--- src/Components/Molecules/footer-icons/footer-icons.js
+++ src/Components/Molecules/footer-icons/footer-icons.js
@@ -1,49 +1,10 @@
 
 import jsonFile from "src/db.json";
 import "../footer-icons/footer-icons.scss";
-import React, { useEffect } from "react";
+import React from "react";
 
 const FooterIcons = () => {
     const footerIcons = jsonFile.footer.footerIcons;
-
-    // const getIcon = () => {
-    //     fetch('https://github.com/Ne-anna/Renome-react/blob/main/src/db.json'
-    //         , {
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //                 'Access-Control-Allow-Origin': '*',
-    //             },
-    //             mode: 'no-cors'
-    //         }
-    //     )
-    //         .then(response => {
-    //             // console.log(response)
-    //             response.json();
-    //         })
-    //         .then(data => {
-    //             console.log(data);
-    //         })
-    //         .catch((error) => {
-    //             console.error(error)
-    //         });
-    // }
-    // useEffect(() => {
-    //     getIcon()
-    // })
-
-    const getIcon = async () => {
-        const requestURL = "../../../db.json";
-        const request = new Request(requestURL);
-
-        await fetch(request)
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data)
-            });
-    }
-    useEffect(() => {
-        getIcon()
-    }, [])
 
     return (
         <div className="footer__social-media">
