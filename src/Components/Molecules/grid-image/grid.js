92fbff2db99f4c2217acef02f875635f1c8d743b
Ne-anna <annabusele@inbox.lv>
2022-06-16 12:19:28 +0300

    Fetch data for overlapping images

--- src/Components/Molecules/grid-image/grid.js
+++ src/Components/Molecules/grid-image/grid.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './grid.scss';
 
 const Grid = (props) => {
+
+    const getData = async () => {
+        const requestURL = "https://raw.githubusercontent.com/Ne-anna/Renome-react/main/src/db.json";
+        const request = new Request(requestURL);
+
+        await fetch(request)
+            .then((response) => response.json())
+            .then((data) => {
+                document.querySelector('#top').src = data.about.images.bottom
+                document.querySelector('#bottom').src = data.about.images.top
+                document.querySelector('#top').alt = data.about.images.altTagFront
+                document.querySelector('#bottom').alt = data.about.images.altTagBack
+            });
+    }
+    useEffect(() => {
+        getData()
+    }, [])
+
     return (
-        <div className="about__image-grid">
-            <img src={props.images.top} alt={props.images.altTagBack} className="about__back-image" />
-            <img src={props.images.bottom} alt={props.images.altTagFront} className="about__front-image" />
+        <div className="about__image-grid" id="app">
+            <img src="" alt="" className="about__back-image" id="bottom" />
+            <img src="" alt="" className="about__front-image" id="top" />
         </div>
     );
 }
