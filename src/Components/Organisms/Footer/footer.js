75f9d6183e3abd8c2e620f624f156429d16a6ddc
Ne-anna <annabusele@inbox.lv>
2022-06-21 19:15:35 +0300

    Re-write fetch code for footer

--- src/Components/Organisms/Footer/footer.js
+++ src/Components/Organisms/Footer/footer.js
@@ -1,21 +1,21 @@
 import FooterIcons from 'src/Components/Molecules/footer-icons/footer-icons';
 import '../Footer/footer.scss';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Footer = (props) => {
+const Footer = () => {
+
+    const [items, setItems] = useState([]);
 
     const getData = async () => {
-        const requestURL = "https://raw.githubusercontent.com/Ne-anna/Renome-react/Fetch-data/src/db.json";
-        const request = new Request(requestURL);
 
+        const requestURL = "https://raw.githubusercontent.com/Ne-anna/Renome-react/main/src/db.json";
+        const request = new Request(requestURL);
         await fetch(request)
             .then((response) => response.json())
             .then((data) => {
-                document.querySelector('#footerTitle').innerHTML = data.footer.title
-                document.querySelector('#logo').innerHTML = data.footer.logo
-                document.querySelector('#copyright').innerHTML = data.footer.copyright
+                setItems(data.footer);
             });
-    }
+    };
 
     useEffect(() => {
         getData()
@@ -23,11 +23,14 @@ const Footer = (props) => {
     return (
         <div className="footer">
             <h5 className="footer__title" id="footerTitle">
+                {items.title}
             </h5>
             <FooterIcons />
             <h2 className="footer__logo" id="logo">
+                {items.logo}
             </h2>
             <h5 className="footer__copyright" id="copyright">
+                {items.copyright}
             </h5>
         </div>
 
