52859db9698d7942b2a4d9acde1164bc18f01a22
Ne-anna <annabusele@inbox.lv>
2022-06-22 15:35:07 +0300

    Remove code dublication and pass as a props

--- src/Components/Molecules/gallery-images/gallery-images.js
+++ src/Components/Molecules/gallery-images/gallery-images.js
@@ -1,34 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './gallery-images.scss'
 
-const GalleryImages = () => {
-
-    const [items, setItems] = useState([]);
-
-    const getData = async () => {
-
-        const requestURL = "https://raw.githubusercontent.com/Ne-anna/Renome-react/main/src/db.json";
-        const request = new Request(requestURL);
-        await fetch(request)
-            .then((response) => response.json())
-            .then((data) => {
-                setItems(data.gallery.galleryImages);
-            });
-    };
-
-    useEffect(() => {
-        getData()
-    }, [])
+const GalleryImages = (props) => {
 
     return (
         <div className="gallery__image-container">
             {
-                items.map((items, index) => {
+                   props.data && props.data.map((items, index) => {
                     return (
                         <img src={items.path} alt={items.altTag} key={index} />
                     )
-                })
-            }
+                    })
+                }
         </div>
     );
 }
--- src/Components/Organisms/Gallery/gallery.js
+++ src/Components/Organisms/Gallery/gallery.js
@@ -1,24 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import './gallery.scss';
+import getData from '../../../fetchlist.js';
 import GalleryImages from 'src/Components/Molecules/gallery-images/gallery-images';
 
 const Gallery = () => {
 
     const [items, setItems] = useState([]);
 
-    const getData = async () => {
-
-        const requestURL = "https://raw.githubusercontent.com/Ne-anna/Renome-react/main/src/db.json";
-        const request = new Request(requestURL);
-        await fetch(request)
-            .then((response) => response.json())
-            .then((data) => {
-                setItems(data.gallery);
-            });
-    };
-
     useEffect(() => {
         getData()
+            .then(items => setItems(items.gallery))
     }, [])
 
     return (
@@ -29,7 +20,7 @@ const Gallery = () => {
             <h5 className="gallery__subtitle" id="gallerySubTitle">
                 {items.subTitle}
             </h5>
-            <GalleryImages />
+            <GalleryImages data = {items.galleryImages}/>
         </div>
     );
 }
